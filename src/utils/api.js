f2b2e45c7df9cdc07a689892a867d164b2ccd661
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-04-05 12:26:38 -0700

    fixed misspelling of axios

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,5 @@
 // Dependencies
-const { default: axios } = require('axios');
-const axois = require('axios');
+const axios = require('axios');
 
 // API URL prefix for all endpoints
 const URL_PREFIX = 'https://rns-app-tracker-api.herokuapp.com/'
@@ -9,7 +8,7 @@ const URL_PREFIX = 'https://rns-app-tracker-api.herokuapp.com/'
 const API = {
     signup: userData => {
         return axios.post(`${URL_PREFIX}/users/new`, userData)
-    }, 
+    },
     login: userData => {
         return axios.post(`${URL_PREFIX}/users`, userData)
     },
@@ -19,28 +18,28 @@ const API = {
                 authorization: `Bearer: ${token}`
             }
         });
-    }, 
+    },
     createApp: (appData, token) => {
         return axios.post(`${URL_PREFIX}/applications`, appData, {
             headers: {
                 authorization: `Bearer: ${token}`
             }
         });
-    }, 
+    },
     getApp: token => {
         return axios.get(`${URL_PREFIX}/applications`, {
             headers: {
                 authorization: `Bearer: ${token}`
             }
         });
-    }, 
+    },
     updateApp: (id, appData, token) => {
         return axios.put(`${URL_PREFIX}/applications/${id}`, appData, {
             headers: {
                 authorization: `Bearer: ${token}`
             }
         });
-    }, 
+    },
     deleteApp: (id, token) => {
         return axios.delete(`${URL_PREFIX}/applications/${id}`, {
             headers: {
