a823cd0caca3832520bdf07e000f3a2f9ca6d1d9
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-04-07 13:39:46 -0700

    added customized greeting to applications page

--- src/pages/Applications.js
+++ src/pages/Applications.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import store from '../config/store';
+import { connect } from 'react-redux';
 
-const Applications = () => {
+const Applications = (props) => {
     return (
         <div>
             <h1>Here's your dumb application page, dummy.</h1>
+            <h2>Welcome, {props.username}</h2>
         </div>
     )
 }
 
-export default Applications;
+const mapStateToProps = (state) => {
+    return {
+        ...state.user
+    }
+}
+
+export default connect(mapStateToProps)(Applications)
